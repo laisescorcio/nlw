71351f780e14a4f21ff590f116109e51ca459bbd
Eduardo Godoy <eduardo.godoy@agenciaeconverse.com.br>
2020-06-07 01:04:04 -0300

    Projeto Coleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,42 @@
+// Objetos possuem propriedades e fucnionalidade
+
+// const pessoa = {
+//   altura: "1,80",
+//   idade: 24,
+//   solteiro: true,
+//   correr() {
+//     document.write("executar funcao correr")
+//   }
+
+// }
+// pessoa.correr()
+
+// Array - Vetores -> Colecao de dados
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+// document.write(colecaoDeBolinhas[3].name)
+// Acessou o nome pela posicao
+
+// Funcoes - Funcionalidades - Atalhos - Reuso de codigo
+// Registrar funcao
+// function sayMyName(name) {
+//   document.write(name)
+// }
+// // Executar funcao
+// sayMyName("Escorcio")
+// sayMyName("Correia")
+
+// condicionais
+// const notaFinal = 7
+
+// if ( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+//  loop repeticoes
+// for ( i = 0 ; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+
+// teste
